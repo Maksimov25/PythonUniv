35ee8983e38a5f3310c44182730bb463e92a60f0
Maksimov25 <alexmax_2015@mail.ru>
2022-11-30 23:03:47 +0300

    ("-")

--- Practical work/field of miracles(3 work)/__init__.py
+++
@@ -1,5 +0,0 @@
-from .d_manager import *
-from .round import *
-from .main import *
-from .word_handler import *
-from .difficult import *
---
+++ Practical_work/field_of_miracles/__init__.py
@@ -0,0 +1,5 @@
+from Practical_work.field_of_miracles.d_manager import *
+from Practical_work.field_of_miracles.round import *
+from Practical_work.field_of_miracles.main import *
+from Practical_work.field_of_miracles.word_handler import *
+from Practical_work.field_of_miracles.difficult import *
