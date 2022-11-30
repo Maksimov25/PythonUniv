f6dd3c0fa0dc9e294143e1faecb243a97bf577ca
Maksimov25 <alexmax_2015@mail.ru>
2022-11-30 19:55:56 +0300

    ("-")

---
+++ Module work/Module 6/task 1.py
@@ -0,0 +1,5 @@
+list = [s for s in input().split()]
+for i in list:
+    if int(i) % 2 == 0:
+        list.remove(i)
+print(" ".join(list))
---
+++ Module work/Module 6/task 7.py
@@ -0,0 +1,9 @@
+list = [s for s in input().split()]
+list_gen = []
+for i in range(0, len(list)):
+    if list_gen._contains_(list[i]):
+        print("ДА")
+    else:
+        print("НЕТ")
+
+    list_gen.append(list[i])
