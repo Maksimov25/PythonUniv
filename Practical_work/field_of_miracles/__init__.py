5d411ffe1c1443daec0ea9bbdb9b945ccb9dec72
Maksimov25 <39645914+Maksimov25@users.noreply.github.com>
2022-12-13 10:20:05 +0300

    Delete Practical_work/field_of_miracles directory

--- Practical_work/field_of_miracles/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-
-from Practical_work.field_of_miracles.d_manager import *
-from Practical_work.field_of_miracles.round import *
-from Practical_work.field_of_miracles.main import *
-from Practical_work.field_of_miracles.word_handler import *
-from Practical_work.field_of_miracles.difficult import *
